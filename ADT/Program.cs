0119320be06868736c5661d98f64dd2368b0169b
Rotav <10harrob@gmail.com>
2015-11-25 12:29:41 +0000

    Intial

---
+++ ADT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(9);
+            Console.WriteLine(stack);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADT/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADT
+{
+    class Stack
+    {
+        private int size;
+        private int[] items;
+        private int head;
+
+        public Stack(int theSize)
+        {
+            this.size = theSize;
+            this.head = 0;
+            this.items = new int[size];
+        }
+
+        public void push(int newValue) 
+        {
+            if (head == size)
+            {
+                Console.WriteLine("The array is full ");
+            }
+            else
+            {
+                items[head++] = newValue;
+            }
+        }
+        public int pop() 
+        {
+            if (head == 0)
+            {
+                Console.WriteLine("The array is empty.");
+                return -1;
+            }
+            else
+            {
+                return items[--head];
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder details = new StringBuilder();
+            details.AppendLine("Stack contains");
+            foreach (var item in items)
+            {
+                details.AppendLine(items.ToString());
+            }
+            return details.ToString();
+        }
+    }
+}
